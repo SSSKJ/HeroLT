9a8b242441d9639c1e6b2a8116c6832c3b73db4e
SSSKJ <438841268@qq.com>
2023-06-08 03:35:24 +0800

    add GCN model for ImGAGN

---
+++ framework/nn/Models/GCN.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from Layers import GraphConvolution
+from Modules import Attention
+
+
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout, generate_node, min_node):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
+        self.gc3 = GraphConvolution(nhid, 2)
+        self.attention = Attention(nfeat*2, 1)
+        self.generate_node = generate_node
+        self.min_node = min_node
+        self.dropout = dropout
+        self.eps = 1e-10
+
+    def forward(self, x, adj):
+
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x1 = self.gc2(x, adj)
+        x2 = self.gc3(x, adj)
+        return F.log_softmax(x1, dim=1), F.log_softmax(x2, dim=1), F.softmax(x1, dim=1)[:,-1]
+
+    def get_embedding(self,x , adj):
+        x = F.relu(self.gc1(x, adj))
+        x = torch.spmm(adj, x)
+        return x
