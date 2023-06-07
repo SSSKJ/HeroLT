18a5cc85d7063dc89e1728529d283789b4d2744f
SSSKJ <438841268@qq.com>
2023-06-08 03:16:54 +0800

    add utils functions for Decoupling

---
+++ framework/utils/__init__.py
@@ -0,0 +1,186 @@
+import importlib
+import numpy as np
+import os
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from sklearn.metrics import f1_score
+
+def source_import(file_path):
+    """This function imports python module directly from source code using importlib"""
+    spec = importlib.util.spec_from_file_location('', file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+def class_count(data: DataLoader):
+    
+    labels = np.array(data.dataset.labels)
+    class_data_num = []
+    for l in np.unique(labels):
+        class_data_num.append(len(labels[labels == l]))
+    return class_data_num
+
+def special_mkdir(root, name):
+
+    if name not in os.listdir(root):
+        os.mkdir(f'{root}/{name}')
+
+def torch2numpy(x):
+    if isinstance(x, torch.Tensor):
+        return x.detach().cpu().numpy()
+    elif isinstance(x, (list, tuple)):
+        return tuple([torch2numpy(xi) for xi in x])
+    else:
+        return x
+    
+def mic_acc_cal(preds, labels):
+    if isinstance(labels, tuple):
+        assert len(labels) == 3
+        targets_a, targets_b, lam = labels
+        acc_mic_top1 = (lam * preds.eq(targets_a.data).cpu().sum().float() \
+                       + (1 - lam) * preds.eq(targets_b.data).cpu().sum().float()) / len(preds)
+    else:
+        acc_mic_top1 = (preds == labels).sum().item() / len(labels)
+    return acc_mic_top1
+
+def weighted_mic_acc_cal(preds, labels, ws):
+    acc_mic_top1 = ws[preds == labels].sum() / ws.sum()
+    return acc_mic_top1
+
+def logits2score(logits, labels):
+    scores = F.softmax(logits, dim=1)
+    score = scores.gather(1, labels.view(-1, 1))
+    score = score.squeeze().cpu().numpy()
+    return score
+
+def logits2entropy(logits):
+    scores = F.softmax(logits, dim=1)
+    scores = scores.cpu().numpy() + 1e-30
+    ent = -scores * np.log(scores)
+    ent = np.sum(ent, 1)
+    return ent
+
+
+def logits2CE(logits, labels):
+    scores = F.softmax(logits, dim=1)
+    score = scores.gather(1, labels.view(-1, 1))
+    score = score.squeeze().cpu().numpy() + 1e-30
+    ce = -np.log(score)
+    return ce
+
+def get_priority(ptype, logits, labels):
+    if ptype == 'score':
+        ws = 1 - logits2score(logits, labels)
+    elif ptype == 'entropy':
+        ws = logits2entropy(logits)
+    elif ptype == 'CE':
+        ws = logits2CE(logits, labels)
+    
+    return ws
+
+def shot_acc(preds, labels, train_data, many_shot_thr=100, low_shot_thr=20, acc_per_cls=False):
+    
+    if isinstance(train_data, np.ndarray):
+        training_labels = np.array(train_data).astype(int)
+    else:
+        training_labels = np.array(train_data.dataset.labels).astype(int)
+
+    if isinstance(preds, torch.Tensor):
+        preds = preds.detach().cpu().numpy()
+        labels = labels.detach().cpu().numpy()
+    elif isinstance(preds, np.ndarray):
+        pass
+    else:
+        raise TypeError('Type ({}) of preds not supported'.format(type(preds)))
+    train_class_count = []
+    test_class_count = []
+    class_correct = []
+    for l in np.unique(labels):
+        train_class_count.append(len(training_labels[training_labels == l]))
+        test_class_count.append(len(labels[labels == l]))
+        class_correct.append((preds[labels == l] == labels[labels == l]).sum())
+
+    many_shot = []
+    median_shot = []
+    low_shot = []
+    for i in range(len(train_class_count)):
+        if train_class_count[i] > many_shot_thr:
+            many_shot.append((class_correct[i] / test_class_count[i]))
+        elif train_class_count[i] < low_shot_thr:
+            low_shot.append((class_correct[i] / test_class_count[i]))
+        else:
+            median_shot.append((class_correct[i] / test_class_count[i]))    
+ 
+    if len(many_shot) == 0:
+        many_shot.append(0)
+    if len(median_shot) == 0:
+        median_shot.append(0)
+    if len(low_shot) == 0:
+        low_shot.append(0)
+
+    if acc_per_cls:
+        class_accs = [c / cnt for c, cnt in zip(class_correct, test_class_count)] 
+        return np.mean(many_shot), np.mean(median_shot), np.mean(low_shot), class_accs
+    else:
+        return np.mean(many_shot), np.mean(median_shot), np.mean(low_shot)
+    
+def weighted_shot_acc(preds, labels, ws, train_data, many_shot_thr=100, low_shot_thr=20):
+    
+    training_labels = np.array(train_data.dataset.labels).astype(int)
+
+    if isinstance(preds, torch.Tensor):
+        preds = preds.detach().cpu().numpy()
+        labels = labels.detach().cpu().numpy()
+    elif isinstance(preds, np.ndarray):
+        pass
+    else:
+        raise TypeError('Type ({}) of preds not supported'.format(type(preds)))
+    train_class_count = []
+    test_class_count = []
+    class_correct = []
+    for l in np.unique(labels):
+        train_class_count.append(len(training_labels[training_labels == l]))
+        test_class_count.append(ws[labels==l].sum())
+        class_correct.append(((preds[labels==l] == labels[labels==l]) * ws[labels==l]).sum())
+
+    many_shot = []
+    median_shot = []
+    low_shot = []
+    for i in range(len(train_class_count)):
+        if train_class_count[i] > many_shot_thr:
+            many_shot.append((class_correct[i] / test_class_count[i]))
+        elif train_class_count[i] < low_shot_thr:
+            low_shot.append((class_correct[i] / test_class_count[i]))
+        else:
+            median_shot.append((class_correct[i] / test_class_count[i]))          
+    return np.mean(many_shot), np.mean(median_shot), np.mean(low_shot)
+
+
+def F_measure(preds, labels, theta=None):
+    
+    return f1_score(labels.detach().cpu().numpy(), preds.detach().cpu().numpy(), average='macro')
+
+def init_weights(model, weights_path, caffe=False, classifier=False):  
+    """Initialize weights"""
+    print('Pretrained %s weights path: %s' % ('classifier' if classifier else 'feature model',
+                                              weights_path))    
+    weights = torch.load(weights_path)   
+    if not classifier:
+        if caffe:
+            weights = {k: weights[k] if k in weights else model.state_dict()[k] 
+                       for k in model.state_dict()}
+        else:
+            weights = weights['state_dict_best']['feat_model']
+            weights = {k: weights['module.' + k] if 'module.' + k in weights else model.state_dict()[k] 
+                       for k in model.state_dict()}
+    else:      
+        weights = weights['state_dict_best']['classifier']
+        weights = {k: weights['module.fc.' + k] if 'module.fc.' + k in weights else model.state_dict()[k] 
+                   for k in model.state_dict()}
+    model.load_state_dict(weights)   
+    return model
+
