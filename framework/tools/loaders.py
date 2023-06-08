7c98c4ae78b490deb066e185235f6a3c8616c579
SSSKJ <438841268@qq.com>
2023-06-08 15:47:57 +0800

    define ImGAGN dataloader

--- framework/tools/loaders.py
+++ framework/tools/loaders.py
@@ -1,7 +1,10 @@
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
-from datasets import LT_Dataset
+from .datasets import *
+from utils import *
+
+import numpy as np
 
 # Data Loader definiation from Decoupling
 class Decoupling_loader:
@@ -98,7 +101,65 @@ class Decoupling_loader:
                             shuffle=shuffle, num_workers=num_workers)
         
 
+
+class ImGAGN_loader:
+
+    @staticmethod
+    def load_data(ratio_generated, data):
+
+        dataset = Graph_Dataset(root="data", dataset = args.dataset, is_normalize = args.is_normalize, add_self_loop = args.add_sl)
         
+        features, edges, labels, idx_test = data.features.cpu(), data.edge_index.cpu(), data.labels.cpu(), data.idx_test.cpu()
+        idx_train = torch.LongTensor([x for x in range(len(labels)) if x not in idx_test])
+
+        for i in range(max(labels)):
+            chosen = idx_train[(labels == (max(labels) - i))[idx_train]]
+            if chosen.shape[0] > 0:
+                break
+        majority = np.array([x for x in idx_train if labels[x] != max(labels) - i])
+        minority = np.array([x for x in idx_train if labels[x] == max(labels) - i])
+
+        num_minority = minority.shape[0]
+        num_majority = majority.shape[0]
+        print("Number of majority: ", num_majority)
+        print("Number of minority: ", num_minority)
+
+        generate_node = []
+        generate_label=[]
+        for i in range(labels.shape[0], labels.shape[0]+int(ratio_generated*num_majority)-num_minority):
+            generate_node.append(i)
+            generate_label.append(1)
+        idx_train= np.hstack((idx_train, np.array(generate_node)))
+        print(idx_train.shape)
+
+        # minority_test = np.array([x for x in idx_test if labels[x] in smallest_class])
+        minority_test = np.array([x for x in idx_test if labels[x] == max(labels) - i])
+        minority_all = np.hstack((minority, minority_test))
+
+        labels= np.hstack((labels, np.array(generate_label)))
+
+        adj_real = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                                shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+        # adj_real = utils.torch_sparse_tensor_to_sparse_mx(adj.to_sparse(), (labels.shape[0], labels.shape[0]))
+        adj = adj_real + adj_real.T.multiply(adj_real.T > adj_real) - adj_real.multiply(adj_real.T > adj_real)
+
+        features = normalize(features)
+        features = torch.FloatTensor(features)
+        adj = normalize(adj + sp.eye(adj.shape[0]))
+        adj = sparse_mx_to_torch_sparse_tensor(adj)
+        labels = torch.LongTensor(labels)
+
+        idx_train = torch.LongTensor(idx_train)
+        idx_test = torch.LongTensor(idx_test)
+        generate_node = torch.LongTensor(np.array(generate_node))
+        minority = torch.LongTensor(minority)
+        majority = torch.LongTensor(majority)
+        minority_all = torch.LongTensor(minority_all)
+
+        return adj, adj_real, features, labels, idx_train, idx_test, generate_node, minority, majority, minority_all
+
+
+
         
 
 
