1534f66eb704637e990e72a96ef6d682bc56a4d1
SSSKJ <438841268@qq.com>
2023-06-09 12:03:24 +0800

    define graph loader and imgagn data processor

--- framework/tools/loaders.py
+++ framework/tools/loaders.py
@@ -102,14 +102,85 @@ class Decoupling_loader:
         
 
 
-class ImGAGN_loader:
+class Graph_loader:
 
-    @staticmethod
-    def load_data(ratio_generated, data):
-
-        dataset = Graph_Dataset(root="data", dataset = args.dataset, is_normalize = args.is_normalize, add_self_loop = args.add_sl)
+    @classmethod
+    def load_data(self, config, dataset, model_name, root, device = -1):
         
-        features, edges, labels, idx_test = data.features.cpu(), data.edge_index.cpu(), data.labels.cpu(), data.idx_test.cpu()
+        self.config = config
+        self.config['device'] = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            
+        # Load data - Cora, CiteSeer, cora_full
+        self._dataset = Graph_Dataset(root = root, dataset = dataset, is_normalize = self.config['is_normalize'], add_self_loop = self.config['add_sl'])
+        self.edge_index = self._dataset.edge_index
+
+        adj = self._dataset.adj
+        features = self._dataset.features
+        labels = self._dataset.labels
+        class_sample_num = 20
+        im_class_num = self.config['im_class_num']
+
+        # Natural Setting
+        if self.config['im_ratio'] == 1:
+            self.config['criterion'] = 'mean'
+            labels, og_to_new = refine_label_order(labels)
+            idx_train, idx_val, idx_test, class_num_mat = split_natural(labels, og_to_new)
+            samples_per_label = torch.tensor(class_num_mat[:,0])
+
+        # Set embeder
+        if model_name == 'lte4g':
+            idx_train_set_class, ht_dict_class = separate_ht(samples_per_label, labels, idx_train, method = self.config['sep_class'], manual = False)
+            idx_train_set, degree_dict, degrees, above_head, below_tail  = separate_class_degree(adj, idx_train_set_class, below = self.config['sep_degree'])
+            
+            idx_val_set = separate_eval(idx_val, labels, ht_dict_class, degrees, above_head, below_tail)
+            idx_test_set = separate_eval(idx_test, labels, ht_dict_class, degrees, above_head, below_tail)
+
+            self.config['sep_point'] = len(ht_dict_class['H'])
+
+            self.idx_train_set_class = idx_train_set_class
+            self.degrees = degrees
+            self.above_head = above_head
+            self.below_tail = below_tail
+
+            print('Above Head Degree:', above_head)
+            print('Below Tail Degree:', below_tail)
+            
+            self.idx_train_set = {}
+            self.idx_val_set = {}
+            self.idx_test_set = {}
+            for sep in ['HH', 'HT', 'TH', 'TT']:
+                self.idx_train_set[sep] = idx_train_set[sep].to(self.config['device'])
+                self.idx_val_set[sep] = idx_val_set[sep].to(self.config['device'])
+                self.idx_test_set[sep] = idx_test_set[sep].to(self.config['device'])
+
+        if model_name != 'tailgnn':
+            adj = normalize_adj(adj) if self.config['adj_norm_1'] else normalize_sym(adj)
+
+        self.adj = adj.to(self.config['device'])
+        self.features = features.to(self.config['device'])
+        self.labels = labels.to(self.config['device'])
+        self.class_sample_num = class_sample_num
+        self.im_class_num = im_class_num
+
+        self.idx_train = idx_train.to(self.config['device'])
+        self.idx_val = idx_val.to(self.config['device'])
+        self.idx_test = idx_test.to(self.config['device'])
+
+        self.samples_per_label = samples_per_label
+        self.class_num_mat = class_num_mat
+        print(class_num_mat)
+
+        self.config['nfeat'] = features.shape[1]
+        self.config['nclass'] = labels.max().item() + 1
+        self.config['im_class_num'] = im_class_num
+
+        return self.config, eval(f'self.__{model_name}_data_preprocessor')
+
+    def __imgagn_data_preprocessor(self):
+        
+        ratio_generated = self.config['ratio_generated']
+
+        features, edges, labels, idx_test = self.features.cpu(), self.edge_index.cpu(), self.labels.cpu(), self.idx_test.cpu()
         idx_train = torch.LongTensor([x for x in range(len(labels)) if x not in idx_test])
 
         for i in range(max(labels)):
@@ -132,7 +203,6 @@ class ImGAGN_loader:
         idx_train= np.hstack((idx_train, np.array(generate_node)))
         print(idx_train.shape)
 
-        # minority_test = np.array([x for x in idx_test if labels[x] in smallest_class])
         minority_test = np.array([x for x in idx_test if labels[x] == max(labels) - i])
         minority_all = np.hstack((minority, minority_test))
 
@@ -140,7 +210,7 @@ class ImGAGN_loader:
 
         adj_real = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
                                 shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
-        # adj_real = utils.torch_sparse_tensor_to_sparse_mx(adj.to_sparse(), (labels.shape[0], labels.shape[0]))
+
         adj = adj_real + adj_real.T.multiply(adj_real.T > adj_real) - adj_real.multiply(adj_real.T > adj_real)
 
         features = normalize(features)
