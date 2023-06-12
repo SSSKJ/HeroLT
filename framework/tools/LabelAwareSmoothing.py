f0d4b9cd36481e80f49d646a198802145738c265
SSSKJ <438841268@qq.com>
2023-06-12 16:02:11 +0800

    define LabelAwareSmoothing

---
+++ framework/tools/LabelAwareSmoothing.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LabelAwareSmoothing(nn.Module):
+    def __init__(self, cls_num_list, smooth_head, smooth_tail, shape='concave', power=None):
+        super(LabelAwareSmoothing, self).__init__()
+
+        n_1 = max(cls_num_list)
+        n_K = min(cls_num_list)
+
+        if shape == 'concave':
+            self.smooth = smooth_tail + (smooth_head - smooth_tail) * np.sin((np.array(cls_num_list) - n_K) * np.pi / (2 * (n_1 - n_K)))
+
+        elif shape == 'linear':
+            self.smooth = smooth_tail + (smooth_head - smooth_tail) * (np.array(cls_num_list) - n_K) / (n_1 - n_K)
+
+        elif shape == 'convex':
+            self.smooth = smooth_head + (smooth_head - smooth_tail) * np.sin(1.5 * np.pi + (np.array(cls_num_list) - n_K) * np.pi / (2 * (n_1 - n_K)))
+
+        elif shape == 'exp' and power is not None:
+            self.smooth = smooth_tail + (smooth_head - smooth_tail) * np.power((np.array(cls_num_list) - n_K) / (n_1 - n_K), power)
+
+        self.smooth = torch.from_numpy(self.smooth)
+        self.smooth = self.smooth.float()
+        if torch.cuda.is_available():
+            self.smooth = self.smooth.cuda()
+
+    def forward(self, x, target):
+        smoothing = self.smooth[target]
+        confidence = 1. - smoothing
+        logprobs = F.log_softmax(x, dim=-1)
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = confidence * nll_loss + smoothing * smooth_loss
+
+        return loss.mean()
