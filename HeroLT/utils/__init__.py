abc5ac341aa2c8b2d912bb5fa4df562592cfbf8e
SSSKJ <438841268@qq.com>
2023-06-14 04:04:35 +0800

    move functions to utils and rename topk_accuracy

--- HeroLT/utils/__init__.py
+++ HeroLT/utils/__init__.py
@@ -1030,7 +1030,7 @@ def mixup_criterion(criterion, pred, y_a, y_b, lam):
     return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
 
 
-def accuracy(output, target, topk=(1,)):
+def topk_accuracy(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
         maxk = max(topk)
@@ -1045,3 +1045,90 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
+    
+from matplotlib import pyplot as plt
+
+
+class FusionMatrix(object):
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+        self.reset()
+
+    def reset(self):
+        self.matrix = np.zeros((self.num_classes, self.num_classes), dtype=int)
+
+    def update(self, output, label):
+        length = output.shape[0]
+        for i in range(length):
+            self.matrix[output[i], label[i]] += 1
+
+    def get_rec_per_class(self):
+        rec = np.array(
+            [
+                self.matrix[i, i] / self.matrix[:, i].sum()
+                for i in range(self.num_classes)
+            ]
+        )
+        rec[np.isnan(rec)] = 0
+        return rec
+
+    def get_pre_per_class(self):
+        pre = np.array(
+            [
+                self.matrix[i, i] / self.matrix[i, :].sum()
+                for i in range(self.num_classes)
+            ]
+        )
+        pre[np.isnan(pre)] = 0
+        return pre
+
+    def get_accuracy(self):
+        acc = (
+            np.sum([self.matrix[i, i] for i in range(self.num_classes)])
+            / self.matrix.sum()
+        )
+        return acc
+
+    def plot_confusion_matrix(self, normalize = False, cmap=plt.cm.Blues):
+
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+        # Compute confusion matrix
+        cm = self.matrix.T
+
+        fig, ax = plt.subplots()
+        im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+        ax.figure.colorbar(im, ax=ax)
+        # We want to show all ticks...
+        ax.set(xticks=np.arange(cm.shape[1]),
+               yticks=np.arange(cm.shape[0]),
+               # ... and label them with the respective list entries
+               xticklabels=np.arange(self.num_classes), yticklabels=np.arange(self.num_classes),
+               title=title,
+               ylabel='True label',
+               xlabel='Predicted label')
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+                 rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i in range(cm.shape[0]):
+            for j in range(cm.shape[1]):
+                ax.text(j, i, format(cm[i, j], fmt),
+                        ha="center", va="center",
+                        color="white" if cm[i, j] > thresh else "black")
+        fig.tight_layout()
+        return fig
+
+
+def accuracy(output, label):
+    cnt = label.shape[0]
+    true_count = (output == label).sum()
+    now_accuracy = true_count / cnt
+    return now_accuracy, cnt
