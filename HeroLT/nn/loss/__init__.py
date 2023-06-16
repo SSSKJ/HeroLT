f2df2ec48ab44c95e4aab50ed5ad7ba5888407fd
SSSKJ <438841268@qq.com>
2023-06-16 22:27:59 +0800

    rename loss

--- HeroLT/nn/loss/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .DiscCentroidsLoss import *
-# from .SoftmaxLoss import *
-# from .BalancedSoftmaxLoss import BalancedSoftmax
-from .CrossEntropy import CrossEntropy
-from .CSCE import CSCE
-from .LDAMLoss import LDAMLoss
