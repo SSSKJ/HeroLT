b0dfdd98caa3dee1245f51b110e3c0ecad5e26e3
SSSKJ <438841268@qq.com>
2023-06-19 21:57:28 +0800

    update dataset list

--- HeroLT/data/__init__.py
+++ HeroLT/data/__init__.py
@@ -1,16 +1,16 @@
 ## dataset names and links
-dataset_list = {'cora-full': '',
-            'email': '',
-            'wiki': '',
-            'amazon-clothing': '',
-            'amazon-electronics': '',
-            'imagenet-lt': '',
-            'places-lt': '',
-            'inatural2018': '',
-            'cifar10-lt': '',
-            'cifar100-lt': '',
-            'lvisv1.0': '',
-            'eurlex-4k': '',
-            'amazoncat-13k': '',
-            'wiki10-31k': ''
-            }
+dataset_list = ['cora-full', 
+                'email', 
+                'wiki', 
+                'amazon-clothing',
+                'amazon-electronics',
+                'imagenet-lt',
+                'places-lt',
+                'inatural2018',
+                'cifar10-lt',
+                'cifar100-lt',
+                'lvisv1.0',
+                'eurlex-4k',
+                'amazoncat-13k',
+                'wiki10-31k'
+                ]
