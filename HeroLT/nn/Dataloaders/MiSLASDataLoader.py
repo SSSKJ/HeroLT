40d6a0e45f10cbc0c20468c6d46ef516c92303a0
SSSKJ <438841268@qq.com>
2023-06-26 02:58:38 +0800

    define MiSLASDataLoader

---
+++ HeroLT/nn/Dataloaders/MiSLASDataLoader.py
@@ -0,0 +1,238 @@
+from ..Datasets.MiSLASDataset import IMBALANCECIFAR10, IMBALANCECIFAR100, LT_Dataset, LT_Dataset_Eval
+from ..Samplers import ClassAwareSampler
+
+import torch
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+
+class CIFAR100_LT(object):
+    def __init__(self, distributed, root='./data/cifar100', imb_type='exp',
+                    imb_factor=0.01, batch_size=128, num_works=40):
+
+        train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        
+
+        eval_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        
+        
+        train_dataset = IMBALANCECIFAR100(root=root, imb_type=imb_type, imb_factor=imb_factor, rand_number=0, train=True, download=True, transform=train_transform)
+        eval_dataset = torchvision.datasets.CIFAR100(root=root, train=False, download=True, transform=eval_transform)
+        
+        self.cls_num_list = train_dataset.get_cls_num_list()
+
+        self.dist_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset) if distributed else None
+        self.train_instance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=True,
+            num_workers=num_works, pin_memory=True, sampler=self.dist_sampler)
+
+        balance_sampler = ClassAwareSampler(train_dataset)
+        self.train_balance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True, sampler=balance_sampler)
+
+        self.eval = DataLoader(
+            eval_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True)
+        
+
+class CIFAR10_LT(object):
+
+    def __init__(self, distributed, root='./data/cifar10', imb_type='exp',
+                    imb_factor=0.01, batch_size=128, num_works=40):
+
+        train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        
+        eval_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        
+        
+        train_dataset = IMBALANCECIFAR10(root=root, imb_type=imb_type, imb_factor=imb_factor, rand_number=0, train=True, download=True, transform=train_transform)
+        eval_dataset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=eval_transform)
+        
+        self.cls_num_list = train_dataset.get_cls_num_list()
+
+        self.dist_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset) if distributed else None
+        self.train_instance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=True,
+            num_workers=num_works, pin_memory=True, sampler=self.dist_sampler)
+
+        balance_sampler = ClassAwareSampler(train_dataset)
+        self.train_balance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True, sampler=balance_sampler)
+
+        self.eval = DataLoader(
+            eval_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True)
+        
+
+class Places_LT(object):
+    def __init__(self, distributed, root="", batch_size=60, num_works=40):
+        
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        
+        transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0),
+            transforms.ToTensor(),
+            normalize,
+            ])
+        
+
+        transform_test = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                normalize,
+            ])
+        
+        train_txt = "./datasets/data_txt/Places_LT_train.txt"
+        eval_txt = "./datasets/data_txt/Places_LT_test.txt"
+
+        
+        train_dataset = LT_Dataset(root, train_txt, transform=transform_train)
+        eval_dataset = LT_Dataset_Eval(root, eval_txt, transform=transform_test, class_map=train_dataset.class_map)
+        
+        self.cls_num_list = train_dataset.cls_num_list
+
+        self.dist_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset) if distributed else None
+        self.train_instance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=True,
+            num_workers=num_works, pin_memory=True, sampler=self.dist_sampler)
+
+        balance_sampler = ClassAwareSampler(train_dataset)
+        self.train_balance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True, sampler=balance_sampler)
+
+        self.eval = DataLoader(
+            eval_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True)
+
+class iNatural2018(object):
+    def __init__(self, distributed, root="", batch_size=60, num_works=40):
+        
+        normalize = transforms.Normalize(mean=[0.466, 0.471, 0.380], std=[0.195, 0.194, 0.192])
+        
+        transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0),
+            transforms.ToTensor(),
+            normalize,
+            ])
+        
+
+        transform_test = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                normalize,
+            ])
+
+        train_txt = "./datasets/data_txt/iNaturalist18_train.txt"
+        eval_txt = "./datasets/data_txt/iNaturalist18_val.txt"
+        
+        train_dataset = LT_Dataset(root, train_txt, transform=transform_train)
+        eval_dataset = LT_Dataset_Eval(root, eval_txt, transform=transform_test, class_map=train_dataset.class_map)
+        
+        self.cls_num_list = train_dataset.cls_num_list
+
+        self.dist_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset) if distributed else None
+        self.train_instance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=True,
+            num_workers=num_works, pin_memory=True, sampler=self.dist_sampler)
+
+        balance_sampler = ClassAwareSampler(train_dataset)
+        self.train_balance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True, sampler=balance_sampler)
+
+        self.eval = DataLoader(
+            eval_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True)
+        
+class ImageNet_LT(object):
+    def __init__(self, distributed, root="", batch_size=60, num_works=40):
+        
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        
+        transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0),
+            transforms.ToTensor(),
+            normalize,
+            ])
+        
+
+        transform_test = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                normalize,
+            ])
+
+        
+        train_txt = "./datasets/data_txt/ImageNet_LT_train.txt"
+        eval_txt = "./datasets/data_txt/ImageNet_LT_test.txt"
+        
+        train_dataset = LT_Dataset(root, train_txt, transform=transform_train)
+        eval_dataset = LT_Dataset_Eval(root, eval_txt, transform=transform_test, class_map=train_dataset.class_map)
+        
+        self.cls_num_list = train_dataset.cls_num_list
+
+        self.dist_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset) if distributed else None
+        self.train_instance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=True,
+            num_workers=num_works, pin_memory=True, sampler=self.dist_sampler)
+
+        balance_sampler = ClassAwareSampler(train_dataset)
+        self.train_balance = DataLoader(
+            train_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True, sampler=balance_sampler)
+
+        self.eval = DataLoader(
+            eval_dataset,
+            batch_size=batch_size, shuffle=False,
+            num_workers=num_works, pin_memory=True)
+        
+
+
+        
+
+
